3cfb5e69fdeb5af22eb1e878e9dac502adc06465
xumingle <382417655@qq.com>
2018-10-20 02:38:44 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).on('click','.button1',function(){
+    $('.center').css('background','url(./images/center1.png) no-repeat')
+}).on('click','.button2',function(){
+    $('.center').css('background','url(./images/center2.png) no-repeat')
+}).on('click','.title',function(){
+    $('#home').fadeIn()
+}).on('click','.button-to-index',function(){
+    $('#home').fadeOut()
+    $('.num').each(function(index,el){
+        new CountUp(el,0,Number($(el).text().replace(/,+/g,'')),0,5).start()
+    })
+})
+
+$('.num').each(function(index,el){
+    new CountUp(el,0,Number($(el).text().replace(/,+/g,'')),0,5).start()
+})
